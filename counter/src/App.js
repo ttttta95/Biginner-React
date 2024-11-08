26beeed74762bb468a3f6866462a7ef603af513c
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-08 10:45:16 +0900

    카운터 완성

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import Controller from "./component/Controller";
 import Veiwer from "./component/Viewer";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Veiwer />
+        <Veiwer count={count}/>
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount}/>
       </section>
     </div>
   )
