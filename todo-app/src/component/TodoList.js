4ca7a5a5a5800877201a9fa5a570b014008e7ac5
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-09 18:22:12 +0900

    UI 완성

--- todo-app/src/component/TodoList.js
+++ todo-app/src/component/TodoList.js
@@ -1,3 +1,4 @@
+import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = () => {
@@ -5,6 +6,11 @@ const TodoList = () => {
         <div className="TodoList">
             <h4>🌱 Todo List 🌱</h4>
             <input className="searchbar" placeholder="검색어를 입력하세요" />
+            <div className="list_wrapper">
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div>
         </div>
     );
 };
