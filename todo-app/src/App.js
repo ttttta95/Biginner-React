adf8892980d732715520fc47fae4b4effabaeda4
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-09 20:45:46 +0900

    예예

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -44,7 +44,7 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <TodoEditor />
+            <TodoEditor onCreate={onCreate}/>
             <TodoList />
         </div>
     );
--- todo-app/src/component/TodoEditor.js
+++ todo-app/src/component/TodoEditor.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./TodoEditor.css";
 
-const TodoEditor = () => {
+const TodoEditor = ({ onCreate }) => {
+    const [content, setContent] = useState("");
+    const onChangeContent = () => {
+        setContent(e.target.value);
+    };
+
     return (
         <div className="TodoEditor">
             <h4>새로운 할 일을 입력해주세요</h4>
             <div className="editor_wrapper">
-                <input placeholder="할 일을 입력해주세요" />
+                <input 
+                 value={content}
+                 onChange={onChangeContent}
+                 placeholder="할 일을 입력해주세요"
+                />
                 <button>추가</button>
             </div>
         </div>
