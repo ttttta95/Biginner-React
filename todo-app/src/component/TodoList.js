3d69e309c13c8974f8a8aa89c1a3f44a44cef08f
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-11-10 10:49:45 +0900

    할 일 렌더링

--- todo-app/src/component/TodoList.js
+++ todo-app/src/component/TodoList.js
@@ -1,15 +1,15 @@
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
-const TodoList = () => {
+const TodoList = ({ todo }) => {
     return (
         <div className="TodoList">
             <h4>🌱 Todo List 🌱</h4>
             <input className="searchbar" placeholder="검색어를 입력하세요" />
             <div className="list_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {todo.map((it) => (
+                    <div>{it.content}</div>
+                ))}
             </div>
         </div>
     );
